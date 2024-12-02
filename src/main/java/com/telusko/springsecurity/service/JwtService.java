704b9c430dd48d4ec284ae221fcff99af48c5cfb
Dileep9123 <gunduboinadileep@gmail.com>
2024-12-02 14:54:18 +0530

    tc

--- src/main/java/com/telusko/springsecurity/service/JwtService.java
+++ src/main/java/com/telusko/springsecurity/service/JwtService.java
@@ -24,7 +24,7 @@ import io.jsonwebtoken.security.Keys;
 @Service
 public class JwtService {
 	
-	 private static final String SECRET = "TmV3U2VjcmV0S2V5Rm9ySldUU2lnbmluZ1B1cnBvc2VzMTIzNDU2Nzg=\r\n";
+
 
 	    private String secretKey;
 
